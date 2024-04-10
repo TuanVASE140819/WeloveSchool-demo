27e2fd2045c0c5a0b3d10e15580cfd645f0e6fd0
Vu Anh Tuan <tuanvuanhse140819@gmail.com>
2024-04-10 14:53:39 +0700

    up code

--- src/components/detailSchool/DetailSchool.js
+++ src/components/detailSchool/DetailSchool.js
@@ -716,87 +716,90 @@ const DetailSchool = () => {
                     </div>
                     <div className="flex flex-row">
                       <div className="basis-2/3 text-[#6B6B6B] mt-4">
-                        <div
-                          className="
+                        {/* flex flex-row */}
+                        <div className="flex flex-row">
+                          <div
+                            className="
                     w-28 h-10 inline-block mr-2"
-                        >
-                          <div class=" basis-1/2 flex flex-row items-center justify-center">
-                            <div class="basis-1/3">
-                              <div className="text-[#A3A3A3] font-bold">
-                                8815
+                          >
+                            <div class=" basis-1/2 flex flex-row items-center justify-center">
+                              <div class="basis-1/3">
+                                <div className="text-[#A3A3A3] font-bold">
+                                  8815
+                                </div>
                               </div>
-                            </div>
-                            <div class="basis-2/3">
-                              <svg
-                                width="32px"
-                                height="32px"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                              >
-                                <rect width="24" height="24" fill="white" />
-                                <path
-                                  fill-rule="evenodd"
-                                  clip-rule="evenodd"
-                                  d="M5 9.5C5 7.01472 7.01472 5 9.5 5C11.9853 5 14 7.01472 14 9.5C14 11.9853 11.9853 14 9.5 14C7.01472 14 5 11.9853 5 9.5Z"
-                                  fill="#A3A3A3"
-                                />
-                                <path
-                                  d="M14.3675 12.0632C14.322 12.1494 14.3413 12.2569 14.4196 12.3149C15.0012 12.7454 15.7209 13 16.5 13C18.433 13 20 11.433 20 9.5C20 7.567 18.433 6 16.5 6C15.7209 6 15.0012 6.2546 14.4196 6.68513C14.3413 6.74313 14.322 6.85058 14.3675 6.93679C14.7714 7.70219 15 8.5744 15 9.5C15 10.4256 14.7714 11.2978 14.3675 12.0632Z"
-                                  fill="#A3A3A3"
-                                />
-                                <path
-                                  fill-rule="evenodd"
-                                  clip-rule="evenodd"
-                                  d="M4.64115 15.6993C5.87351 15.1644 7.49045 15 9.49995 15C11.5112 15 13.1293 15.1647 14.3621 15.7008C15.705 16.2847 16.5212 17.2793 16.949 18.6836C17.1495 19.3418 16.6551 20 15.9738 20H3.02801C2.34589 20 1.85045 19.3408 2.05157 18.6814C2.47994 17.2769 3.29738 16.2826 4.64115 15.6993Z"
-                                  fill="#A3A3A3"
-                                />
-                                <path
-                                  d="M14.8185 14.0364C14.4045 14.0621 14.3802 14.6183 14.7606 14.7837V14.7837C15.803 15.237 16.5879 15.9043 17.1508 16.756C17.6127 17.4549 18.33 18 19.1677 18H20.9483C21.6555 18 22.1715 17.2973 21.9227 16.6108C21.9084 16.5713 21.8935 16.5321 21.8781 16.4932C21.5357 15.6286 20.9488 14.9921 20.0798 14.5864C19.2639 14.2055 18.2425 14.0483 17.0392 14.0008L17.0194 14H16.9997C16.2909 14 15.5506 13.9909 14.8185 14.0364Z"
-                                  fill="#A3A3A3"
-                                />
-                              </svg>
+                              <div class="basis-2/3">
+                                <svg
+                                  width="32px"
+                                  height="32px"
+                                  viewBox="0 0 24 24"
+                                  fill="none"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                >
+                                  <rect width="24" height="24" fill="white" />
+                                  <path
+                                    fill-rule="evenodd"
+                                    clip-rule="evenodd"
+                                    d="M5 9.5C5 7.01472 7.01472 5 9.5 5C11.9853 5 14 7.01472 14 9.5C14 11.9853 11.9853 14 9.5 14C7.01472 14 5 11.9853 5 9.5Z"
+                                    fill="#A3A3A3"
+                                  />
+                                  <path
+                                    d="M14.3675 12.0632C14.322 12.1494 14.3413 12.2569 14.4196 12.3149C15.0012 12.7454 15.7209 13 16.5 13C18.433 13 20 11.433 20 9.5C20 7.567 18.433 6 16.5 6C15.7209 6 15.0012 6.2546 14.4196 6.68513C14.3413 6.74313 14.322 6.85058 14.3675 6.93679C14.7714 7.70219 15 8.5744 15 9.5C15 10.4256 14.7714 11.2978 14.3675 12.0632Z"
+                                    fill="#A3A3A3"
+                                  />
+                                  <path
+                                    fill-rule="evenodd"
+                                    clip-rule="evenodd"
+                                    d="M4.64115 15.6993C5.87351 15.1644 7.49045 15 9.49995 15C11.5112 15 13.1293 15.1647 14.3621 15.7008C15.705 16.2847 16.5212 17.2793 16.949 18.6836C17.1495 19.3418 16.6551 20 15.9738 20H3.02801C2.34589 20 1.85045 19.3408 2.05157 18.6814C2.47994 17.2769 3.29738 16.2826 4.64115 15.6993Z"
+                                    fill="#A3A3A3"
+                                  />
+                                  <path
+                                    d="M14.8185 14.0364C14.4045 14.0621 14.3802 14.6183 14.7606 14.7837V14.7837C15.803 15.237 16.5879 15.9043 17.1508 16.756C17.6127 17.4549 18.33 18 19.1677 18H20.9483C21.6555 18 22.1715 17.2973 21.9227 16.6108C21.9084 16.5713 21.8935 16.5321 21.8781 16.4932C21.5357 15.6286 20.9488 14.9921 20.0798 14.5864C19.2639 14.2055 18.2425 14.0483 17.0392 14.0008L17.0194 14H16.9997C16.2909 14 15.5506 13.9909 14.8185 14.0364Z"
+                                    fill="#A3A3A3"
+                                  />
+                                </svg>
+                              </div>
                             </div>
                           </div>
-                        </div>
 
-                        <div
-                          className="
+                          <div
+                            className="
                     w-5 h-10 inline-block mr-2"
-                        >
-                          <div class="flex flex-row items-center justify-center">
-                            <div class="basis-1/3">
-                              <div className="text-[#A3A3A3] font-bold">
-                                8815
+                          >
+                            <div class="flex flex-row items-center justify-center">
+                              <div class="basis-1/3">
+                                <div className="text-[#A3A3A3] font-bold">
+                                  8815
+                                </div>
                               </div>
-                            </div>
-                            <div class="basis-2/3">
-                              <svg
-                                width="28px"
-                                height="28px"
-                                viewBox="0 0 32 32"
-                                version="1.1"
-                                xmlns="http://www.w3.org/2000/svg"
-                              >
-                                <g
-                                  id="Page-1"
-                                  stroke="none"
-                                  stroke-width="1"
-                                  fill="none"
-                                  fill-rule="evenodd"
+                              <div class="basis-2/3">
+                                <svg
+                                  width="28px"
+                                  height="28px"
+                                  viewBox="0 0 32 32"
+                                  version="1.1"
+                                  xmlns="http://www.w3.org/2000/svg"
                                 >
                                   <g
-                                    id="Icon-Set-Filled"
-                                    transform="translate(-154.000000, -881.000000)"
-                                    fill="#A3A3A3"
+                                    id="Page-1"
+                                    stroke="none"
+                                    stroke-width="1"
+                                    fill="none"
+                                    fill-rule="evenodd"
                                   >
-                                    <path
-                                      d="M186,893.244 L174.962,891.56 L170,881 L165.038,891.56 L154,893.244 L161.985,901.42 L160.095,913 L170,907.53 L179.905,913 L178.015,901.42 L186,893.244"
-                                      id="start-favorite"
-                                    ></path>
+                                    <g
+                                      id="Icon-Set-Filled"
+                                      transform="translate(-154.000000, -881.000000)"
+                                      fill="#A3A3A3"
+                                    >
+                                      <path
+                                        d="M186,893.244 L174.962,891.56 L170,881 L165.038,891.56 L154,893.244 L161.985,901.42 L160.095,913 L170,907.53 L179.905,913 L178.015,901.42 L186,893.244"
+                                        id="start-favorite"
+                                      ></path>
+                                    </g>
                                   </g>
-                                </g>
-                              </svg>
+                                </svg>
+                              </div>
                             </div>
                           </div>
                         </div>
